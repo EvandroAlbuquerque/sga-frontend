c6e19f0825614b90e5553776b9aab80b20c27a03
Evandro Albuquerque <evandrobotelhoalbuquerque@gmail.com>
2019-10-03 12:15:23 -0300

    Configuration fixes to action and volunteer components and action service. Ignore all vscode files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActionDetailComponent } from './action-detail/action-detail.component';
+import { ActionListComponent } from './action-list/action-list.component';
+import { VolunteerDetailComponent } from './volunteer-detail/volunteer-detail.component';
+import { VolunteerListComponent } from './volunteer-list/volunteer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActionDetailComponent,
+    ActionListComponent,
+    VolunteerDetailComponent,
+    VolunteerListComponent
   ],
   imports: [
     BrowserModule,
