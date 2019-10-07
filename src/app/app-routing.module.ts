eee4f6644dfa3e514a2d0a27ff36d289dd110b3c
Evandro Albuquerque <evandrobotelhoalbuquerque@gmail.com>
2019-10-07 08:31:38 -0300

    Start creation routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ActionListComponent } from './components/action/action-list/action-list.component';
+import { InstitutionListComponent } from './components/institution/institution-list/institution-list.component';
+import { NeedyPointListComponent } from './components/needy-point/needy-point-list/needy-point-list.component';
+import { VolunteerListComponent } from './components/volunteer/volunteer-list/volunteer-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'acao', component: ActionListComponent },
+  { path: 'instituicao', component: InstitutionListComponent },
+  { path: 'pontocarente', component: NeedyPointListComponent },
+  { path: 'voluntario', component: VolunteerListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = [
+  ActionListComponent,
+  InstitutionListComponent,
+  NeedyPointListComponent,
+  VolunteerListComponent
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ActionDetailComponent } from './components/action/action-detail/action-detail.component';
-import { ActionListComponent } from './components/action/action-list/action-list.component';
-import { VolunteerDetailComponent } from './components/volunteer/volunteer-detail/volunteer-detail.component';
-import { VolunteerListComponent } from './components/volunteer/volunteer-list/volunteer-list.component';
-import { InstitutionListComponent } from './components/institution/institution-list/institution-list.component';
-import { InstitutionDetailComponent } from './components/institution/institution-detail/institution-detail.component';
-import { NeedyPointListComponent } from './components/needy-point/needy-point-list/needy-point-list.component';
-import { NeedyPointDetailComponent } from './components/needy-point/needy-point-detail/needy-point-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ActionDetailComponent,
-    ActionListComponent,
-    VolunteerDetailComponent,
-    VolunteerListComponent,
-    InstitutionListComponent,
-    InstitutionDetailComponent,
-    NeedyPointListComponent,
-    NeedyPointDetailComponent
+    routingComponent
   ],
   imports: [
     BrowserModule,
