429659c507a738121c14b995d85eb48185990d0d
Evandro Albuquerque <evandro.albuquerque@codata.pb.gov.br>
2019-11-20 11:25:17 -0300

    Add Action Reactive Form.

--- src/app/components/action/action-list/action-list.component.ts
+++ src/app/components/action/action-list/action-list.component.ts
@@ -3,19 +3,20 @@ import { ActionService } from '../../../services/action/action.service';
 import { Router } from '@angular/router';
 import { VolunteerType } from '../../volunteer/volunteer-list/volunteer-list.component';
 import { VolunteerService } from 'src/app/services/volunteer/volunteer.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
-export type ActionType = {
+export interface ActionType {
   id: number;
-  responsible: String;
-  description: String;
+  responsible: number;
+  description: string;
   place: {
-    postalCode: String;
-    street: String;
+    postalCode: string;
+    street: string;
     number: number;
-    district: String;
-  }
-  hour: String;
-  obs: String;
+    district: string;
+  };
+  hour: string;
+  obs: string;
   current: boolean;
 }
 
@@ -33,14 +34,34 @@ export class ActionListComponent implements OnInit {
   // ]
 
   actions: Array<ActionType>;
-  volunteers: Array<VolunteerType>
+  // action: ActionType;
+  volunteers: Array<VolunteerType>;
   showForm = false;
+
+  actionForm = new FormGroup({
+    responsible: new FormControl(''),
+    description: new FormControl(''),
+    hour: new FormControl(''),
+    obg: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      number: new FormControl(''),
+      postalCode: new FormControl(''),
+      district: new FormControl('')
+    })
+  });
 
   constructor(private service: ActionService, private volunteerService: VolunteerService, private router: Router) {}
 
   ngOnInit() {
     this.service.getActions().subscribe(actions => this.actions = actions);
     this.volunteerService.getVolunteers().subscribe(volunteers => this.volunteers = volunteers);
+  }
+
+  addAction(action: ActionType): void {
+    action = this.actionForm.value;
+    // tslint:disable-next-line: no-shadowed-variable
+    this.service.postAction(action).subscribe(action => this.actions.push(action));
   }
 
   // openAction(id: number) {
